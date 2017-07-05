c15460ec55ddb4e7ba086d7b3317e0312432bcfd
Rodrigo <pmello.rodrigor@gmail.com>
2017-07-05 00:01:14 -0300

    changed the methods that load and create property assertion, added parameters, create assertion still partial hard coded

--- SistemaDeApoio/src/main/java/model/OntologyManager.java
+++ SistemaDeApoio/src/main/java/model/OntologyManager.java
@@ -27,82 +27,115 @@ public class OntologyManager {
 	private OWLOntologyManager manager;
 	private IRI documentIRI;
 	private OWLOntology localOntology;
-	
+
 	public OntologyManager() {
-		
+
 	}
 
-	public void loadOntology() throws OWLOntologyCreationException {
-		/*
-		 * Since we wont load our ontology from web, we have to load the file
-		 * that contains the ontology
-		 */
+	public void loadOntology(String path) throws OWLOntologyCreationException {
+		/* Since we wont load our ontology from web, we have to load from a file */
 		this.manager = OWLManager.createOWLOntologyManager();
 		// TODO: find the current directory and make this path relative
-		File file = new File(
-				"/home/rr/workspace/aplicacao_ontologia/SistemaDeApoio/ontology/ontologia_aplicacao.owl");
+		File file = new File(path);
 		// loading the local ontology
 		this.localOntology = manager.loadOntologyFromOntologyDocument(file);
-		
+
 		System.out.println("Loaded ontology: " + localOntology);
 		// We can always obtain the location where an ontology was loaded from
 		this.documentIRI = manager.getOntologyDocumentIRI(localOntology);
 		System.out.println("    from: " + documentIRI);
 	}
-	
-	public void createPropertyAssertions(String proprerty) throws Exception {
-        // We can specify the properties of an individual using property
-        
-        OWLDataFactory factory = manager.getOWLDataFactory();
-        
-        OWLIndividual investidor = factory.getOWLNamedIndividual(IRI.create(documentIRI+ "#investidor"));
-        OWLIndividual banco = factory.getOWLNamedIndividual(IRI.create(documentIRI+ "#bancos"));
-        OWLObjectProperty temConhecimento = factory.getOWLObjectProperty(IRI.create(documentIRI+ "#tem_conhecimento_previo"));
-        
-                
-        OWLObjectPropertyAssertionAxiom propertyAssertion = factory.getOWLObjectPropertyAssertionAxiom(temConhecimento, investidor, banco);
-        AddAxiom addAxiomChange = new AddAxiom(localOntology, propertyAssertion);
-        manager.applyChange(addAxiomChange);
-        
-        
-		OWLClass personClass = factory.getOWLClass(IRI.create(documentIRI+ "#Investidor"));
-        OWLClassAssertionAxiom ax = factory.getOWLClassAssertionAxiom(personClass, investidor);
-        // Add this axiom to our ontology - with a convenience method
-        manager.addAxiom(localOntology, ax);
-        
-        manager.saveOntology(localOntology, new StreamDocumentTarget(new ByteArrayOutputStream()));
-        saveOntology();
-}	
-	public void saveOntology() throws IOException, OWLOntologyStorageException{
-		 File output = File.createTempFile("new_ontology", "owl");
-	        IRI documentIRI2 = IRI.create(output);
-	        // save in OWL/XML format
-	        manager.saveOntology(localOntology, new OWLXMLOntologyFormat(), documentIRI2);
-	        // save in RDF/XML
-	        manager.saveOntology(localOntology, documentIRI2);
-	        // print out the ontology
-	        StreamDocumentTarget target = new StreamDocumentTarget(
-	                new ByteArrayOutputStream());
-	        manager.saveOntology(localOntology, target);
-	        // Remove the ontology from the manager
-	        manager.removeOntology(localOntology);
+
+	public void createPropertyAssertions() throws Exception {
+		// We can specify the properties of an individual using property
+
+		OWLDataFactory factory = manager.getOWLDataFactory();
+
+		OWLIndividual investidor = factory.getOWLNamedIndividual(IRI
+				.create(documentIRI + "#investidor"));
+		OWLIndividual banco = factory.getOWLNamedIndividual(IRI
+				.create(documentIRI + "#bancos"));
+		OWLObjectProperty temConhecimento = factory.getOWLObjectProperty(IRI
+				.create(documentIRI + "#tem_conhecimento_previo"));
+
+		OWLObjectPropertyAssertionAxiom propertyAssertion = factory
+				.getOWLObjectPropertyAssertionAxiom(temConhecimento,
+						investidor, banco);
+		AddAxiom addAxiomChange = new AddAxiom(localOntology, propertyAssertion);
+		manager.applyChange(addAxiomChange);
+
+		OWLClass personClass = factory.getOWLClass(IRI.create(documentIRI
+				+ "#Investidor"));
+		OWLClassAssertionAxiom ax = factory.getOWLClassAssertionAxiom(
+				personClass, investidor);
+		// Add this axiom to our ontology - with a convenience method
+		manager.addAxiom(localOntology, ax);
+
+		manager.saveOntology(localOntology, new StreamDocumentTarget(
+				new ByteArrayOutputStream()));
+		saveOntology();
 	}
 
-	
+	public void createPropertyAssertions(String proprerty, String instance1,
+			String instance2) throws Exception {
+		// We can specify the properties of an individual using property
+
+		OWLDataFactory factory = manager.getOWLDataFactory();
+
+		OWLIndividual investidor = factory.getOWLNamedIndividual(IRI
+				.create(documentIRI + "#" + instance1));
+		OWLIndividual banco = factory.getOWLNamedIndividual(IRI
+				.create(documentIRI + "#" + instance2));
+		OWLObjectProperty temConhecimento = factory.getOWLObjectProperty(IRI
+				.create(documentIRI + "#" + proprerty));
+
+		OWLObjectPropertyAssertionAxiom propertyAssertion = factory
+				.getOWLObjectPropertyAssertionAxiom(temConhecimento,
+						investidor, banco);
+		AddAxiom addAxiomChange = new AddAxiom(localOntology, propertyAssertion);
+		manager.applyChange(addAxiomChange);
+
+		OWLClass personClass = factory.getOWLClass(IRI.create(documentIRI
+				+ "#investidor"));
+		OWLClassAssertionAxiom ax = factory.getOWLClassAssertionAxiom(
+				personClass, investidor);
+		// Add this axiom to our ontology - with a convenience method
+		manager.addAxiom(localOntology, ax);
+
+		manager.saveOntology(localOntology, new StreamDocumentTarget(
+				new ByteArrayOutputStream()));
+		saveOntology();
+	}
+
+	public void saveOntology() throws IOException, OWLOntologyStorageException {
+		File output = File.createTempFile("new_ontology", "owl");
+		IRI documentIRI2 = IRI.create(output);
+		// save in OWL/XML format
+		manager.saveOntology(localOntology, new OWLXMLOntologyFormat(),
+				documentIRI2);
+		// save in RDF/XML
+		manager.saveOntology(localOntology, documentIRI2);
+		// print out the ontology
+		StreamDocumentTarget target = new StreamDocumentTarget(
+				new ByteArrayOutputStream());
+		manager.saveOntology(localOntology, target);
+		// Remove the ontology from the manager
+		manager.removeOntology(localOntology);
+	}
 
 	public static void main(String[] args) {
-		
-			OntologyManager manager = new OntologyManager();
-			try {
-				manager.loadOntology();
-				manager.createPropertyAssertions("tem_conhecimento_previo");
-				
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+
+		OntologyManager manager = new OntologyManager();
+		try {
+			manager.loadOntology("/home/rr/workspace/aplicacao_ontologia/SistemaDeApoio/ontology/ontologia_aplicacao.owl");
+			manager.createPropertyAssertions("tem_conhecimento_previo", "investidor", "bancos");
+			//manager.createPropertyAssertions();
 			
-			
-		
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }
